57da6195f419742458ed8b72be15fb8b9dde3bd4
limi6n <6nlimitless@gmail.com>
2020-05-30 16:20:08 +0900

    13. 탑

---
+++ 20200530/top.js
@@ -0,0 +1,22 @@
+function solution(heights) {
+  var answer = [];
+  var temp = [];
+
+  for (let i = 0; i < heights.length; i++) {
+    while (temp.length > 0 && heights[temp[temp.length - 1]] <= heights[i]) {
+      temp.pop();
+    }
+
+    if (temp.length > 0) {
+      answer.push(temp[temp.length - 1] + 1);
+    } else {
+      answer.push(0);
+    }
+
+    temp.push(i);
+  }
+
+  return answer;
+}
+
+module.exports = solution;
