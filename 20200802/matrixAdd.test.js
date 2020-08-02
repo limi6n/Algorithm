6fa052f63413cef0b3e44237200462bcd60995cb
limi6n <6nlimitless@gmail.com>
2020-08-02 10:15:50 +0900

    23.행렬의 덧셈

---
+++ 20200802/matrixAdd.test.js
@@ -0,0 +1,19 @@
+const solution = require("./matrixAdd");
+
+test("solution", () => {
+  expect(
+    solution(
+      [
+        [1, 2],
+        [2, 3],
+      ],
+      [
+        [3, 4],
+        [5, 6],
+      ]
+    )
+  ).toStrictEqual([
+    [4, 6],
+    [7, 9],
+  ]);
+});
