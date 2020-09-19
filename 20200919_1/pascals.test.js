27f31a119f365d888f7fddb4b791ea4f817c1ec3
limi6n <6nlimitless@gmail.com>
2020-09-19 15:43:13 +0900

    26.파스칼 삼각형

---
+++ 20200919_1/pascals.test.js
@@ -0,0 +1,11 @@
+const solution = require("./pascalsTriangle");
+
+test("solution", () => {
+  expect(solution(5)).toStrictEqual([
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+  ]);
+});
