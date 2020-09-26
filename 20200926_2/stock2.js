28bc34cc8bfae72c5741d69f7b913d83033de860
limi6n <6nlimitless@gmail.com>
2020-09-26 18:10:00 +0900

    30. Best Time to Buy and Sell Stock2

---
+++ 20200926_2/stock2.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  var result = 0;
+
+  for (var i = 0; i < prices.length; i++) {
+    if (prices[i] < prices[i + 1]) {
+      result += prices[i + 1] - prices[i];
+    }
+  }
+
+  return result;
+};
+
+module.exports = maxProfit;
