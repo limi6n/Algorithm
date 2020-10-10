09cc2f7c15db63883589dac389c8c2c438619496
limi6n <6nlimitless@gmail.com>
2020-10-10 16:03:15 +0900

    32.두개의 큐를 이용하여 스택 구현하기

---
+++ 20201010_3/2queue-stack.js
@@ -0,0 +1,61 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyStack = function () {
+  this.org = [];
+  this.temp = [];
+};
+
+/**
+ * Push element x onto stack.
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function (x) {
+  console.log(this.org);
+  if (this.org.length == 0) {
+    this.org.push(x);
+  } else {
+    while (this.org.length != 0) {
+      this.temp.push(this.org.pop());
+    }
+    this.org.push(x);
+    while (this.temp.length != 0) {
+      this.org.push(this.temp.pop());
+    }
+  }
+};
+
+/**
+ * Removes the element on top of the stack and returns that element.
+ * @return {number}
+ */
+MyStack.prototype.pop = function () {
+  console.log(this.org);
+  return this.org.shift();
+};
+
+/**
+ * Get the top element.
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+  return this.org[0];
+};
+
+/**
+ * Returns whether the stack is empty.
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function () {
+  return this.org.length + this.temp.length == 0 ? true : false;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
---
+++ 20201010_3/README.md
@@ -0,0 +1,22 @@
+# <Implement Stack using Queues>
+
+## 1. 이해
+
+    Implement a last in first out (LIFO) stack using only two queues.
+    The implemented stack should support all the functions of a normal queue (push, top, pop, and empty).
+
+## 2. 계획
+
+    push가 실행 될 때마다 기존 큐에서 임시 큐로 모든 데이터를 옮긴 후 push 가 실행되고 다시 임시 큐에서 기존 큐로 데이터를 옮긴다.
+    pop 이나 top은 기존 큐에서 실행되도록 한다.
+
+## 3. 실행
+
+## 4. 회고
+
+    2개의 스택으로 큐 구현하기를 바탕으로 이번 코드도 구현해보았다.
+    큐는 스택보다 더 생소해서 마지막에 shift 를 써야하는 부분에서 혼자 pop을 쓰고 에러난다고 안된다고 하고있었다..
+    마지막에 top을 위해 제일 첫번째 인자를 불러오기를 했는데 더 좋은 방법이 뭐가 있을까?
+
+    Runtime: 76 ms, faster than 55.04% of JavaScript online submissions for Implement Stack using Queues.
+    Memory Usage: 38.9 MB, less than 8.63% of JavaScript online submissions for Implement Stack using Queues.
