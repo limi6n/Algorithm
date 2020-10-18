057f8f66374d6e13140a555b5e7ad89a8c86a5e9
limi6n <6nlimitless@gmail.com>
2020-10-18 13:37:28 +0900

    35.Intersection of Two Arrays

---
+++ 20201018_2/intersection.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  var result = [];
+
+  nums1.forEach(function (num) {
+    var i = nums2.indexOf(num);
+    if (i > -1) result.push(nums2[i]);
+  });
+
+  return [...new Set(result)];
+};
+
+module.exports = intersection;
