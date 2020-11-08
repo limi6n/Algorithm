ab7c0d6045aef20cb795e1009b6af0090e73d555
limi6n <6nlimitless@gmail.com>
2020-11-08 16:29:44 +0900

    42. Lemonade Change

---
+++ 20201108_3/lemonade.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function (bills) {
+  let fives = 0;
+  let tens = 0;
+
+  if (bills[0] > 5) {
+    return false;
+  } else {
+    for (const bill of bills) {
+      if (bill === 5) fives++;
+      else if (bill === 10) {
+        tens++;
+        fives--;
+      } else {
+        if (tens > 0) {
+          tens--;
+          fives--;
+        } else fives -= 3;
+      }
+      if (fives < 0 || tens < 0) return false;
+    }
+    return true;
+  }
+};
+
+module.exports = lemonadeChange;
---
+++ 20201108_3/lemonade.test.js
@@ -0,0 +1,5 @@
+const solution = require("./lemonade");
+
+test("solution", () => {
+  expect(solution([5, 5, 10, 10, 20])).toStrictEqual(false);
+});
