d534ae1bc6b27b9d4d0782bff0ef108e57d446db
limi6n <6nlimitless@gmail.com>
2021-09-03 00:32:36 +0900

    쇠막대기

--- 2020/20200620/stick.js
+++ 2020/20200620/stick.js
@@ -12,6 +12,6 @@ function solution(arrangement) {
     }
   }
   return answer;
-}
+} 
 
 module.exports = solution;
--- 2020/20200620/stick.test.js
+++ 2020/20200620/stick.test.js
@@ -3,3 +3,4 @@ const solution = require("./stick");
 test("solution", () => {
   expect(solution("()(((()())(())()))(())")).toStrictEqual(17);
 });
+ 
